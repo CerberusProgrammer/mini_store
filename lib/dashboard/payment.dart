f267b5d1b84c17e9862f381bda19888fc6edc834
sazarcode <cerberusprogrammer@gmail.com>
2023-04-25 12:52:50 -0700

    Pending to correct the total to pay.

--- lib/dashboard/payment.dart
+++ lib/dashboard/payment.dart
@@ -15,77 +15,77 @@ class Payment extends StatefulWidget {
 }
 
 class _PaymentState extends State<Payment> {
+  double totalPayment = 0;
+
   @override
   Widget build(BuildContext context) {
+    for (Product product in widget.products) {
+      totalPayment += product.price * product.quantity;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Payment'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              elevation: 10,
-              child: Flexible(
-                child: ListView.builder(
-                    itemCount: widget.products.length,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        leading: CircleAvatar(
-                          backgroundColor:
-                              widget.products[index].color.withAlpha(128),
-                          child: widget.products[index].images.isEmpty
-                              ? Icon(widget.products[index].icon)
-                              : Image(
-                                  image: widget.products[index].images[0],
-                                ),
-                        ),
-                        trailing: Chip(
-                          side: BorderSide.none,
-                          label: Text(
-                            '\$${widget.products[index].price * widget.products[index].quantity}',
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          elevation: 10,
+          child: ListView.builder(
+              itemCount: widget.products.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor:
+                        widget.products[index].color.withAlpha(128),
+                    child: widget.products[index].images.isEmpty
+                        ? Icon(widget.products[index].icon)
+                        : Image(
+                            image: widget.products[index].images[0],
                           ),
-                        ),
-                        subtitle: Row(
-                          children: [
-                            IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  widget.products[index].quantity--;
-                                });
-                              },
-                              icon: const Icon(Icons.remove),
-                            ),
-                            Chip(
-                              side: BorderSide.none,
-                              label: Text('${widget.products[index].quantity}'),
-                            ),
-                            IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    widget.products[index].quantity++;
-                                  });
-                                },
-                                icon: const Icon(Icons.add)),
-                          ],
-                        ),
-                        onTap: () {},
-                        title: Text(widget.products[index].name),
-                      );
-                    }),
-              ),
-            ),
-          ),
-          Spacer(),
-          Card(
-            child: SizedBox(
-              height: 60,
-              child: Text('ad'),
-            ),
-          )
-        ],
+                  ),
+                  trailing: Chip(
+                    side: BorderSide.none,
+                    label: Text(
+                      '\$${widget.products[index].price * widget.products[index].quantity}',
+                    ),
+                  ),
+                  subtitle: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            widget.products[index].quantity--;
+                          });
+                        },
+                        icon: const Icon(Icons.remove),
+                      ),
+                      Chip(
+                        side: BorderSide.none,
+                        label: Text('${widget.products[index].quantity}'),
+                      ),
+                      IconButton(
+                          onPressed: () {
+                            setState(() {
+                              widget.products[index].quantity++;
+                            });
+                          },
+                          icon: const Icon(Icons.add)),
+                    ],
+                  ),
+                  onTap: () {},
+                  title: Text(widget.products[index].name),
+                );
+              }),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        color: Theme.of(context).colorScheme.primary.withAlpha(50),
+        height: 60,
+        child: Row(
+          children: [Chip(label: Text('Total: $totalPayment'))],
+        ),
       ),
     );
   }
